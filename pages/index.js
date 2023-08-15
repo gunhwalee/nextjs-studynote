996fc7f671b5111054585cc4da9881bdf2d7fbf4
gunhwalee <ghlee2588@gmail.com>
2023-08-15 21:24:36 +0900

    page routing: client-side navigation

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 import { useState } from "react";
+import Link from "next/link";
+
 function Header({ title }) {
   return <h1>{title ? title : "Default title"}</h1>;
 }
 
 export default function HomePage() {
-  const names = ["Ada Lovelace", "Grace Hopper", "Margaret Hamilton", "Bob Lee"];
+  const names = [
+    "Ada Lovelace",
+    "Grace Hopper",
+    "Margaret Hamilton",
+    "Bob Lee",
+  ];
 
   const [likes, setLikes] = useState(0);
 
@@ -22,6 +29,10 @@ export default function HomePage() {
       </ul>
 
       <button onClick={handleClick}>Like ({likes})</button>
+
+      <h3>
+        Read <Link href="/posts/first-post">this page!</Link>
+      </h3>
     </div>
   );
 }
